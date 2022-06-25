0fc1238c7b9070e97010b4c155434cda987b6fcf
Krevvetka <krvvko@gmail.com>
2022-06-25 17:09:34 -0400

    added

--- public/index.php
+++ public/index.php
@@ -5,9 +5,49 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="css/norm.css">
+    <link rel="stylesheet" href="css/main.css">
+
     <title>Document</title>
 </head>
-<body>
+<body class="css-selector">
+
+<main>
+
+    <div class="no-scroll-container">
+
+        <div class="form-container">
+
+            <div class="form-container-top">
+                <img src="https://i.pinimg.com/736x/18/53/ad/1853adae05ace68f6979cbbac48af2ec.jpg" width="300" height="300" alt="">
+                <h1 class="food-name">hehe</h1>
+            </div>
+            <div class="form-container-bottom">
+                <p class="description">Description</p>
+                <div class="form-container-bottom-bottom">
+                    <div class="form-container-bottom-bottom-left">Pros Cons</div>
+                    <div class="form-container-bottom-bottom-right">
+                        <div>yes</div>
+                        <div>idk</div>
+                        <div>no</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <!--
+        <script>
+            for (let i = 0; i<=1000; i++) {
+                document.write(i + `<br>`);
+            }
+        </script>
+        -->
+    </div>
+
+</main>
 
 </body>
 </html>
